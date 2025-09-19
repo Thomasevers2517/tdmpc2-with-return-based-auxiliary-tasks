ca2e2423b8c34101e77f85cfeb7423c85600514a
thomasevers2517 <thomasevers9@gmail.com>
2025-09-19 14:21:57 +0200

    fix syntax bug gumbel

--- tdmpc2/common/math.py
+++ tdmpc2/common/math.py
@@ -289,7 +289,11 @@ def distribution_entropy(logits):
 
 
 def gumbel_softmax_sample(p, temperature=1.0, dim=0):
+	"""Sample from the Gumbel-Softmax distribution."""
+	logits = p.log()
+	gumbels = (
+		-torch.empty_like(logits, memory_format=torch.legacy_contiguous_format).exponential_().log()
+	)  # ~Gumbel(0,1)
 	gumbels = (logits + gumbels) / temperature  # ~Gumbel(logits,tau)
 	y_soft = gumbels.softmax(dim)
-	out = y_soft.argmax(-1)
-	return out
+	return y_soft.argmax(-1)
