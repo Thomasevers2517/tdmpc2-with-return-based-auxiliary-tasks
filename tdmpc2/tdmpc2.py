found a bug in actor_critic_only. The rollout was not in nograd, so future value estimates were backpropping into the dynamics head, even tho there was no consistency loss

tdmpc2/tdmpc2.py
@@ -550,19 +550,19 @@ class TDMPC2(torch.nn.Module):
 		# ------------------------------ Latent rollout (consistency) ------------------------------
 		self.model.train()
 		zs = torch.empty(self.cfg.horizon+1, self.cfg.batch_size, self.cfg.latent_dim, device=self.device)  # allocate (T+1,B,L)
-  
+		consistency_loss = torch.tensor(0., device=self.device)
+
 		if actor_critic_only:
 			with maybe_range('Agent/latent_rollout', self.cfg):
 				with torch.no_grad():
 					zs[0] = self.model.encode(obs[0], task)  # initial latent (B,L)
-				consistency_loss = torch.tensor(0., device=self.device)
-				for t in range(self.cfg.horizon):
-					zs[t+1] = self.model.next(zs[t], action[t], task)            # model prediction z_{t+1}
+					for t in range(self.cfg.horizon):
+						#TODO For complex observation for which encoding is expensive, consider applying consistency loss between all states in the trajectory
+						zs[t+1] = self.model.next(zs[t], action[t], task)            # model prediction z_{t+1}
 		else:
 			with maybe_range('Agent/latent_rollout', self.cfg):
 				z = self.model.encode(obs[0], task)  # initial latent (B,L)
 				zs[0] = z
-				consistency_loss = 0.
 				for t, (_a, _target_next_z) in enumerate(zip(action.unbind(0), next_z.unbind(0))):  # iterate T steps
 					z = self.model.next(z, _a, task)            # model prediction z_{t+1}
 					# Consistency MSE between predicted & encoded next latent
@@ -661,7 +661,13 @@ class TDMPC2(torch.nn.Module):
 		if self.cfg.episodic:
 			info.update(math.termination_statistics(torch.sigmoid(termination_pred[-1]), terminated[-1]), non_blocking=True)
 		return total_loss, zs, info
-  
+	
+	def calc_imagine_value_loss(self, zs, task):
+		with maybe_range('Agent/calc_imagine_value_loss', self.cfg):
+			# Compute 1-step TD targets along imagined trajectory
+			with torch.no_grad():
+				# Shifted latent sequence (
+     
 	def _update(self, obs, action, reward, terminated, actor_critic_only, task=None):
 		"""Single gradient update step.
 